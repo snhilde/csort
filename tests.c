b0459d69372fcb64e8ab6d00b3e9a773b3a31787
Hilde N <snhilde@protonmail.com>
2019-08-25 15:07:02 -0400

    make num arrays 64 bits

--- tests.c
+++ tests.c
@@ -1,5 +1,6 @@
 #include "hsort.h"
 #include <stdio.h>
+#include <inttypes.h>
 
 enum types {
 	INT,
@@ -14,11 +15,11 @@ static void print_arr(void *arr, size_t len, enum types type)
 
 	for (i=0; i<len; i++) {
 		if (type == INT) {
-			printf("%d ", *(int *)arr);
-			arr += sizeof(int);
+			printf("%" PRId64 " ", *(int64_t *)arr);
+			arr += sizeof(int64_t);
 		} else if (type == UINT) {
-			printf("%u ", *(unsigned int *)arr);
-			arr += sizeof(unsigned int);
+			printf("%" PRIu64 " ", *(u_int64_t *)arr);
+			arr += sizeof(u_int64_t);
 		} else if (type == CHAR) {
 			printf("%c ", *(char *)arr);
 			arr += sizeof(char);
@@ -33,12 +34,13 @@ static void print_arr(void *arr, size_t len, enum types type)
 
 int main(int argc, char *argv[])
 {
-	int          nums[]   = {234, 2832, 293, -10, 139, 19, 0, -129, -19, 4, 94, 2, 34, 50, 125, 253};
-	unsigned int u_nums[] = {185, 1284, 288, 23, 90, 12, 1, 985, 38, 235, 267, 23, 1, 1, 3945, 95};
-	char         str[]    = "aTj94adaHae43JHaKEh5gajDmv9AjrrR";
+	int64_t   nums[]   = {234, 2832, 293, -10, 139, 19, 0, -129, -19, 4, 94, 2, 34, 50, 125, 253};
+	u_int64_t u_nums[] = {185, 1284, 288, 23, 90, 12, 1, 985, 38, 235, 267, 23, 1, 1, 3945, 95};
+	char      str[]    = "aTj94adaHae43JHaKEh5gajDmv9AjrrR";
 
 	printf("Integers:\n");
 	print_arr(nums, sizeof(nums)/sizeof(*nums), INT);
+	hsort_int(nums, sizeof(nums)/sizeof(*nums), HSORT_INSERTION_SORT|HSORT_ORDER_ASC);
 	printf("\n");
 
 	printf("Unsigned integers:\n");

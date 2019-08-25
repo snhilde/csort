006ec8bc1683e649daf275a52996898e21f2e03c
Hilde N <snhilde@protonmail.com>
2019-08-25 13:38:19 -0400

    typdef enums

--- hsort.h
+++ hsort.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 
-enum hsort_options {
+typedef enum hsort_options {
 	/* Sorting methods */
 	HSORT_INSERTION_SORT = 1 << 0, /* Insertion sort */
 	HSORT_SELECTION_SORT = 1 << 1, /* Selection sort */
@@ -13,13 +13,13 @@ enum hsort_options {
 	/* Sort orders */
 	HSORT_ORDER_ASC      = 1 << 3, /* Ascending order */
 	HSORT_ORDER_DESC     = 1 << 4, /* Descending order */
-};
+} hsort_options_t;
 
-enum hsort_equality {
+typedef enum hsort_equality {
 	HSORT_GT = 1, /* Greater than */
 	HSORT_LT,     /* Less than */
 	HSORT_EQ      /* Equal to */
-};
+} hsort_equality_t;
 
 
 #endif

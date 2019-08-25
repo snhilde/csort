69dcb28a35e6fe01d5f27d4f639723f5b5920d67
Hilde N <snhilde@protonmail.com>
2019-08-25 14:14:38 -0400

    layout declared functions, remove main

--- hsort.c
+++ hsort.c
@@ -1,7 +1,22 @@
 #include "hsort.h"
 
-int main(int argc, char *argv[])
-{
 
-	return EXIT_SUCCESS;
+hsort_return_t hsort_custom(void *arr, size_t len, size_t size, hsort_equality_cb cb)
+{
+	return HSORT_RET_SUCCESS;
+}
+
+hsort_return_t hsort_int(int64_t *arr, size_t len, hsort_options_t options)
+{
+	return HSORT_RET_SUCCESS;
+}
+
+hsort_return_t hsort_uint(u_int64_t *arr, size_t len, hsort_options_t options)
+{
+	return HSORT_RET_SUCCESS;
+}
+
+hsort_return_t hsort_str(char *str, hsort_options_t options)
+{
+	return HSORT_RET_SUCCESS;
 }

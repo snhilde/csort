2e995448a1751633abd1fe013bbb31127d2d812c
Hilde N <snhilde@protonmail.com>
2019-08-25 14:40:43 -0400

    add beginnings of some test cases

--- tests.c
+++ tests.c
@@ -1,9 +1,52 @@
 #include "hsort.h"
 #include <stdio.h>
 
+enum types {
+	INT,
+	UINT,
+	CHAR
+};
+
+
+static void print_arr(void *arr, size_t len, enum types type)
+{
+	unsigned int i;
+
+	for (i=0; i<len; i++) {
+		if (type == INT) {
+			printf("%d ", *(int *)arr);
+			arr += sizeof(int);
+		} else if (type == UINT) {
+			printf("%u ", *(unsigned int *)arr);
+			arr += sizeof(unsigned int);
+		} else if (type == CHAR) {
+			printf("%c ", *(char *)arr);
+			arr += sizeof(char);
+		} else {
+			printf("Unknown type\n");
+			break;
+		}
+	}
+
+	printf("\n");
+}
 
 int main(int argc, char *argv[])
 {
-	printf("done\n");
+	int          nums[]   = {234, 2832, 293, -10, 139, 19, 0, -129, -19, 4, 94, 2, 34, 50, 125, 253};
+	unsigned int u_nums[] = {185, 1284, 288, 23, 90, 12, 1, 985, 38, 235, 267, 23, 1, 1, 3945, 95};
+	char         str[]    = "aTj94adaHae43JHaKEh5gajDmv9AjrrR";
+
+	printf("Integers:\n");
+	print_arr(nums, sizeof(nums)/sizeof(*nums), INT);
+	printf("\n");
+
+	printf("Unsigned integers:\n");
+	print_arr(u_nums, sizeof(u_nums)/sizeof(*u_nums), UINT);
+	printf("\n");
+
+	printf("Characters:\n");
+	print_arr(str, sizeof(str)/sizeof(*str), CHAR);
+
 	return EXIT_SUCCESS;
 }

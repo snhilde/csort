7ee26e8835b870186aaffdbbb15f346ee6a69529
Hilde N <snhilde@protonmail.com>
2019-08-25 13:37:43 -0400

    add hsort_options

--- hsort.h
+++ hsort.h
@@ -4,11 +4,22 @@
 #include <stdlib.h>
 
 
+enum hsort_options {
+	/* Sorting methods */
+	HSORT_INSERTION_SORT = 1 << 0, /* Insertion sort */
+	HSORT_SELECTION_SORT = 1 << 1, /* Selection sort */
+	HSORT_MERGE_SORT     = 1 << 2, /* Merge sort */
+
+	/* Sort orders */
+	HSORT_ORDER_ASC      = 1 << 3, /* Ascending order */
+	HSORT_ORDER_DESC     = 1 << 4, /* Descending order */
+};
+
 enum hsort_equality {
 	HSORT_GT = 1, /* Greater than */
 	HSORT_LT,     /* Less than */
 	HSORT_EQ      /* Equal to */
-}
+};
 
 
 #endif

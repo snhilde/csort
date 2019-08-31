41205d0c79e696a8b8f9188af36ac3341adc42b8
Hilde N <snhilde@protonmail.com>
2019-08-31 12:21:27 -0400

    handle test outcome

--- tests.c
+++ tests.c
@@ -3,7 +3,15 @@
 
 int main(int argc, char *argv[])
 {
-	hsort_test(20, sizeof(int), true, HSORT_MERGE_SORT|HSORT_ORDER_ASC|HSORT_PRINT_BEFORE|HSORT_PRINT_AFTER);
+	hsort_return_t ret;
+
+	ret = hsort_test(20, sizeof(int), true, HSORT_MERGE_SORT|HSORT_ORDER_ASC|HSORT_PRINT_BEFORE|HSORT_PRINT_AFTER);
+	if (ret == HSORT_RET_SUCCESS)
+		printf("Sort was successful\n");
+	else if (ret == HSORT_RET_INVALIDUSE)
+		printf("Check arguments\n");
+	else
+		printf("Sort failed\n");
 
 	return EXIT_SUCCESS;
 }

ea1b4ecfdb9f038f286c477a91c3d77bb8ffd600
Hilde N <snhilde@protonmail.com>
2019-08-31 12:23:29 -0400

    test all current sorting algorithms with larger arrays

--- tests.c
+++ tests.c
@@ -5,9 +5,25 @@ int main(int argc, char *argv[])
 {
 	hsort_return_t ret;
 
-	ret = hsort_test(20, sizeof(int), true, HSORT_MERGE_SORT|HSORT_ORDER_ASC|HSORT_PRINT_BEFORE|HSORT_PRINT_AFTER);
+	ret = hsort_test(2000, sizeof(int), true, HSORT_INSERTION_SORT|HSORT_ORDER_ASC);
 	if (ret == HSORT_RET_SUCCESS)
-		printf("Sort was successful\n");
+		printf("Insertion sort was successful\n");
+	else if (ret == HSORT_RET_INVALIDUSE)
+		printf("Check arguments\n");
+	else
+		printf("Sort failed\n");
+
+	ret = hsort_test(2000, sizeof(int), true, HSORT_SELECTION_SORT|HSORT_ORDER_ASC);
+	if (ret == HSORT_RET_SUCCESS)
+		printf("Selection sort was successful\n");
+	else if (ret == HSORT_RET_INVALIDUSE)
+		printf("Check arguments\n");
+	else
+		printf("Sort failed\n");
+
+	ret = hsort_test(2000, sizeof(int), true, HSORT_MERGE_SORT|HSORT_ORDER_ASC);
+	if (ret == HSORT_RET_SUCCESS)
+		printf("Merge sort was successful\n");
 	else if (ret == HSORT_RET_INVALIDUSE)
 		printf("Check arguments\n");
 	else

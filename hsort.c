2c0e456f6301e380f5a6ab72c915c195260af5ef
Hilde N <snhilde@protonmail.com>
2019-08-31 09:51:41 -0400

    implement hsort_check()

--- hsort.c
+++ hsort.c
@@ -328,6 +328,83 @@ static void *hsort_random_array(size_t len, size_t size, bool is_signed)
 	return array;
 }
 
+static hsort_return_t hsort_check(void *arr_a, void *arr_b, size_t arr_a_len, size_t arr_b_len, size_t a_size, size_t b_size, bool is_signed)
+{
+	unsigned int i;
+	u_int64_t    a;
+	u_int64_t    b;
+
+	/* Quick sanity checks */
+	if (arr_a_len != arr_b_len) {
+		printf("Array lengths do not match\n" "Array A: %zu\n" "Array B: %zu", arr_a_len, arr_b_len);
+		return HSORT_RET_INVALIDUSE;
+	}
+
+	if (a_size != b_size) {
+		printf("Array data sizes do not match\n" "Array A: %zu\n" "Array B: %zu", a_size, b_size);
+		return HSORT_RET_INVALIDUSE;
+	}
+
+	for (i = 0; i < arr_a_len; i++) {
+		/* Get the value at each index. */
+		switch (a_size) {
+			case 1:
+				if (is_signed) {
+					a = *(int8_t *)arr_a;
+					b = *(int8_t *)arr_b;
+				} else {
+					a = *(u_int8_t *)arr_a;
+					b = *(u_int8_t *)arr_b;
+				}
+				break;
+
+			case 2:
+				if (is_signed) {
+					a = *(int16_t *)arr_a;
+					b = *(int16_t *)arr_b;
+				} else {
+					a = *(u_int16_t *)arr_a;
+					b = *(u_int16_t *)arr_b;
+				}
+				break;
+
+			case 4:
+				if (is_signed) {
+					a = *(int32_t *)arr_a;
+					b = *(int32_t *)arr_b;
+				} else {
+					a = *(u_int32_t *)arr_a;
+					b = *(u_int32_t *)arr_b;
+				}
+				break;
+
+			case 8:
+				if (is_signed) {
+					a = *(int64_t *)arr_a;
+					b = *(int64_t *)arr_b;
+				} else {
+					a = *(u_int64_t *)arr_a;
+					b = *(u_int64_t *)arr_b;
+				}
+				break;
+
+			default:
+				return HSORT_RET_INVALIDUSE;
+		}
+
+		/* Make sure that they're equal to each other. */
+		if (a != b)
+			return HSORT_RET_ERROR;
+
+		/* Advance to the next index. */
+		arr_a += a_size;
+		arr_b += b_size;
+	}
+
+	/* If we made it this far, then all values were equal. */
+	return HSORT_RET_SUCCESS;
+}
+
 
 /* --- SORTING ALGORITHMS --- */
 static hsort_return_t hsort_insertion(void *arr, size_t len, size_t size, hsort_equality_cb cb, hsort_options_t options)

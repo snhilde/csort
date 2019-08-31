7594167acef49013b66759940fb64b60703a3bfb
Hilde N <snhilde@protonmail.com>
2019-08-31 13:55:44 -0400

    print time taken to sort

--- tests.c
+++ tests.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
 	hsort_return_t ret;
 
-	ret = hsort_test(20000, sizeof(int), true, HSORT_INSERTION_SORT|HSORT_ORDER_ASC);
+	ret = hsort_test(20000, sizeof(int), true, HSORT_INSERTION_SORT|HSORT_ORDER_ASC|HSORT_PRINT_TIME);
 	if (ret == HSORT_RET_SUCCESS)
 		printf("Insertion sort was successful\n");
 	else if (ret == HSORT_RET_INVALIDUSE)
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	else
 		printf("Sort failed\n");
 
-	ret = hsort_test(20000, sizeof(int), true, HSORT_SELECTION_SORT|HSORT_ORDER_ASC);
+	ret = hsort_test(20000, sizeof(int), true, HSORT_SELECTION_SORT|HSORT_ORDER_ASC|HSORT_PRINT_TIME);
 	if (ret == HSORT_RET_SUCCESS)
 		printf("Selection sort was successful\n");
 	else if (ret == HSORT_RET_INVALIDUSE)
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	else
 		printf("Sort failed\n");
 
-	ret = hsort_test(20000, sizeof(int), true, HSORT_MERGE_SORT|HSORT_ORDER_ASC);
+	ret = hsort_test(20000, sizeof(int), true, HSORT_MERGE_SORT|HSORT_ORDER_ASC|HSORT_PRINT_TIME);
 	if (ret == HSORT_RET_SUCCESS)
 		printf("Merge sort was successful\n");
 	else if (ret == HSORT_RET_INVALIDUSE)

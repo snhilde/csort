649430d631619ebf558dbcf867c5ae8cdfe3041c
Hilde N <snhilde@protonmail.com>
2019-09-01 09:25:59 -0400

    add HSORT_MERGE_SORT_RECURSE to hsort_options_t

--- hsort.h
+++ hsort.h
@@ -14,17 +14,18 @@
 
 typedef enum hsort_options {
 	/* Sorting methods */
-	HSORT_INSERTION_SORT = 1 << 0, /* Insertion sort */
-	HSORT_SELECTION_SORT = 1 << 1, /* Selection sort */
-	HSORT_MERGE_SORT     = 1 << 2, /* Merge sort */
+	HSORT_INSERTION_SORT     = 1 << 0, /* Insertion sort */
+	HSORT_SELECTION_SORT     = 1 << 1, /* Selection sort */
+	HSORT_MERGE_SORT         = 1 << 2, /* Merge sort by stacking */
+	HSORT_MERGE_SORT_RECURSE = 1 << 2, /* Merge sort by recursion */
 
 	/* Sort orders */
-	HSORT_ORDER_ASC      = 1 << 3, /* Ascending order */
-	HSORT_ORDER_DESC     = 1 << 4, /* Descending order */
+	HSORT_ORDER_ASC          = 1 << 3, /* Ascending order */
+	HSORT_ORDER_DESC         = 1 << 4, /* Descending order */
 
-	HSORT_PRINT_BEFORE   = 1 << 5, /* Print array before sorting */
-	HSORT_PRINT_AFTER    = 1 << 6, /* Print array after sorting */
-	HSORT_PRINT_TIME     = 1 << 7, /* Print time taken to sort */
+	HSORT_PRINT_BEFORE       = 1 << 5, /* Print array before sorting */
+	HSORT_PRINT_AFTER        = 1 << 6, /* Print array after sorting */
+	HSORT_PRINT_TIME         = 1 << 7, /* Print time taken to sort */
 } hsort_options_t;
 
 typedef enum hsort_return {

f3d31c66de6765d82b6a1f0b152e7174d862c8b2
Hilde N <snhilde@protonmail.com>
2019-09-01 12:40:22 -0400

    add test for recursive merge sort

--- tests.c
+++ tests.c
@@ -29,5 +29,13 @@ int main(int argc, char *argv[])
 	else
 		printf("Merge sort failed\n");
 
+	ret = hsort_test(20000, sizeof(int), true, HSORT_MERGE_SORT_RECURSE|HSORT_ORDER_DESC|HSORT_PRINT_TIME);
+	if (ret == HSORT_RET_SUCCESS)
+		printf("Recursive merge sort was successful\n");
+	else if (ret == HSORT_RET_INVALIDUSE)
+		printf("Check arguments\n");
+	else
+		printf("Recursive merge sort failed\n");
+
 	return EXIT_SUCCESS;
 }

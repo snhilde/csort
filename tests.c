257dfaea9b2a7b962056a3dbc2ded4870f7e0ed8
Hilde N <snhilde@protonmail.com>
2019-09-02 21:44:58 -0400

    test very large arrays

--- tests.c
+++ tests.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
 	hsort_return_t ret;
 
-	ret = hsort_test(20000, sizeof(int), true, HSORT_INSERTION_SORT|HSORT_ORDER_DESC|HSORT_PRINT_TIME);
+	ret = hsort_test(200000, sizeof(int), true, HSORT_INSERTION_SORT|HSORT_ORDER_DESC|HSORT_PRINT_TIME);
 	if (ret == HSORT_RET_SUCCESS)
 		printf("Insertion sort was successful\n");
 	else if (ret == HSORT_RET_INVALIDUSE)
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	else
 		printf("Insertion sort failed\n");
 
-	ret = hsort_test(20000, sizeof(int), true, HSORT_SELECTION_SORT|HSORT_ORDER_DESC|HSORT_PRINT_TIME);
+	ret = hsort_test(200000, sizeof(int), true, HSORT_SELECTION_SORT|HSORT_ORDER_DESC|HSORT_PRINT_TIME);
 	if (ret == HSORT_RET_SUCCESS)
 		printf("Selection sort was successful\n");
 	else if (ret == HSORT_RET_INVALIDUSE)
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	else
 		printf("Selection sort failed\n");
 
-	ret = hsort_test(20000, sizeof(int), true, HSORT_MERGE_SORT|HSORT_ORDER_DESC|HSORT_PRINT_TIME);
+	ret = hsort_test(200000, sizeof(int), true, HSORT_MERGE_SORT|HSORT_ORDER_DESC|HSORT_PRINT_TIME);
 	if (ret == HSORT_RET_SUCCESS)
 		printf("Merge sort was successful\n");
 	else if (ret == HSORT_RET_INVALIDUSE)
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	else
 		printf("Merge sort failed\n");
 
-	ret = hsort_test(20000, sizeof(int), true, HSORT_MERGE_SORT_RECURSE|HSORT_ORDER_DESC|HSORT_PRINT_TIME);
+	ret = hsort_test(200000, sizeof(int), true, HSORT_MERGE_SORT_RECURSE|HSORT_ORDER_DESC|HSORT_PRINT_TIME);
 	if (ret == HSORT_RET_SUCCESS)
 		printf("Recursive merge sort was successful\n");
 	else if (ret == HSORT_RET_INVALIDUSE)
